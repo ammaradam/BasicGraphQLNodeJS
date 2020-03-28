4ed8db04f2ff1c5fc8c51d011093b0f57cb6d652
Ammar Adam <m.ammaradam@gmail.com>
2020-03-28 20:56:55 +0800

    Commit codes to remote server

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// allow cross-origin requests
+app.use(cors())
+
+mongoose.connect(
+    'mongodb+srv://ammaradam:ammaradam123@cluster0-0dn41.mongodb.net/test?retryWrites=true&w=majority',
+    {useNewUrlParser: true, useUnifiedTopology: true}
+    );
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,                  // or schema.schema
+    graphiql: true
+}))
+
+app.listen(4000, () =>{
+    console.log('now listening for requests on port 4000');
+});
